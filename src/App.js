114bf35389c79e6ab1a1a624d1fec94f74bd9f41
MPTLOKESH <78644257+MPTLOKESH@users.noreply.github.com>
2024-02-02 18:43:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ const App = () => {
       <div className="min-h-screen w-screen relative">
         <Router>
           <Routes>
-            // <Route index element={<Welcome />} />
+          
             <Route index element={<SignIn />} />
             <Route path="/Home" element={<Home />} />
             <Route path="/product/:id" element={<Product />} />
